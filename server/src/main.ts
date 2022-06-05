66f9e1a6525fdc45771de1bc9d1152065553ab53
mikezhen <mike.zhen@icloud.com>
2022-06-04 22:44:48 -0700

    initial server setup with web session cookie

---
+++ server/src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as session from 'express-session';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    abortOnError: false,
+  });
+
+  // Initialize web session middleware
+  app.set('trust proxy', 1);
+  app.use(
+    session({
+      secret: 'my-secret', // Move this to environment variable
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 86400000, // 24 hours
+      },
+    }),
+  );
+
+  await app.listen(3000);
+}
+bootstrap();
