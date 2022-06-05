cbd5d496f9d8ef581a0f06df0abf9485fc403492
mikezhen <mike.zhen@icloud.com>
2022-06-04 22:51:42 -0700

    generate chat module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsModule } from './cats/cats.module';
 import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [CatsModule, ChatModule],
+  imports: [ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
