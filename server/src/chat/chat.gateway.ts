56289de3544f6b95b38e4245eb36dba504a21368
mikezhen <mike.zhen@icloud.com>
2022-06-21 20:39:23 -0700

    generated chat service with websockets

--- server/src/chat/chat.gateway.ts
+++ server/src/chat/chat.gateway.ts
@@ -1,4 +1,12 @@
-import { WebSocketGateway, SubscribeMessage, MessageBody } from '@nestjs/websockets';
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  MessageBody,
+  WsResponse,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { from, map, Observable } from 'rxjs';
+import { Server } from 'socket.io';
 import { ChatService } from './chat.service';
 import { CreateChatDto } from './dto/create-chat.dto';
 import { UpdateChatDto } from './dto/update-chat.dto';
@@ -6,6 +14,23 @@ import { UpdateChatDto } from './dto/update-chat.dto';
 @WebSocketGateway()
 export class ChatGateway {
   constructor(private readonly chatService: ChatService) {}
+
+  @WebSocketServer() server: Server;
+
+  @SubscribeMessage('events')
+  handleEvents(@MessageBody() data: any): Observable<WsResponse<number>> {
+    return from([1, 2, 3]).pipe(map(item => ({ event: 'events', data: item })));
+  }
+
+  @SubscribeMessage('identity')
+  async identity(@MessageBody() data: number): Promise<number> {
+    return data;
+  }
+
+  @SubscribeMessage('broadcast')
+  handleBroadcast(@MessageBody() data: string) {
+    this.server.emit('broadcast', data);
+  }
 
   @SubscribeMessage('createChat')
   create(@MessageBody() createChatDto: CreateChatDto) {
--- server/src/chat/entities/chat.entity.ts
+++ server/src/chat/entities/chat.entity.ts
@@ -1,1 +1,9 @@
-export class Chat {}
+import { Message } from './message.entity';
+
+export class Chat {
+  id: string;
+  createdBy: string;
+  createdAt: Date;
+  recentMessage: Message;
+  members: string[];
+}
