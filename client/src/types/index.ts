858159c415544207641ae19d9e8d23246fbaec2a
mikezhen <mike.zhen@icloud.com>
2022-06-26 01:41:25 -0700

    define all interfaces and types

---
+++ client/src/types/index.ts
@@ -0,0 +1,45 @@
+export interface Address {
+  streetNumber: number;
+  route: string; // Named route such as NE Sesame St
+  locality: string; // City or town
+  region: string; // State or province
+  postalCode: string;
+}
+
+export interface Place {
+  name: string; // Name of the place
+  address: Address;
+  phoneNumber?: string;
+}
+
+export interface Owner {
+  avatarUrl: string; // URL to the avatar image
+  phoneNumbers: string[];
+  emails: string[];
+}
+
+export interface Weight {
+  value: number;
+  unit: MassUnit
+}
+
+export interface Photo {
+  caption: string;
+  imageUrl: string;
+}
+
+export interface Pet {
+  name: string;
+  breed: string;
+  sex: Sex;
+  birthday: Date;
+  color: string;
+  weight: Weight;
+  description: string;
+  photos: Photo[];
+  owner: Owner;
+  veterinarian: Place;
+}
+
+export type MassUnit = 'lb' | 'kg';
+export type Sex = 'Male' | 'Female';
