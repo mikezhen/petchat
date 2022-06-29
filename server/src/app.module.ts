f91673054dbedf5f668e57cb6299672bcfa24a28
mikezhen <mike.zhen@icloud.com>
2022-06-28 23:09:16 -0700

    added new owner resource

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatModule } from './chat/chat.module';
+import { OwnerModule } from './owner/owner.module';
 
 @Module({
-  imports: [ChatModule],
+  imports: [ChatModule, OwnerModule],
   controllers: [AppController],
   providers: [AppService],
 })
