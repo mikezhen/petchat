bccea4056071717b9d299599a111602bb1625238
mikezhen <mike.zhen@icloud.com>
2022-07-04 19:18:48 -0700

    load server code env var from config for local dev

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatModule } from './chat/chat.module';
+import firebaseConfig from './config/firebase.config';
 import { OwnerModule } from './owner/owner.module';
 import { PetModule } from './pet/pet.module';
 
 @Module({
-  imports: [ChatModule, OwnerModule, PetModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [firebaseConfig],
+    }),
+    ChatModule,
+    OwnerModule,
+    PetModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/firebase/firebase.service.ts
+++ server/src/firebase/firebase.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { FirebaseApp, initializeApp } from 'firebase/app';
 import { Firestore, getFirestore } from 'firebase/firestore';
 import { FirebaseStorage, getStorage } from 'firebase/storage';
-import firebaseConfig from './config';
 
 @Injectable()
 export class FirebaseService {
   private firebaseApp: FirebaseApp;
 
-  constructor() {
-    this.firebaseApp = initializeApp(firebaseConfig);
+  constructor(private configService: ConfigService) {
+    this.firebaseApp = initializeApp(configService.get('firebase'));
   }
 
   firestore = (): Firestore => {
