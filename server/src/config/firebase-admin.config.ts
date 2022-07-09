ef52d0d956759ec6ce603d70cefbd72830f40856
mikezhen <mike.zhen@icloud.com>
2022-07-09 15:58:45 -0700

    replace escaped newlines in the private key for admin sdk

--- server/src/config/firebase-admin.config.ts
+++ server/src/config/firebase-admin.config.ts
@@ -4,5 +4,5 @@ import { env } from 'process';
 export default registerAs('firebase-admin', () => ({
   projectId: env.FIREBASE_PROJECT_ID,
   clientEmail: env.FIREBASE_ADMIN_SDK_CLIENT_EMAIL,
-  privateKey: env.FIREBASE_ADMIN_SDK_PRIVATE_KEY,
+  privateKey: env.FIREBASE_ADMIN_SDK_PRIVATE_KEY.replace(/\\n/g, '\n'),
 }));
