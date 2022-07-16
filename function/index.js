d787e856f23f216afd2e9c87545f8f6de50c5521
Mike Zhen <mike.zhen@icloud.com>
2022-07-16 02:31:24 -0700

    Cloud Function: Resize Images (#13)
    * function code to process images
    
    * update workflow jobs to deploy functions
    
    * fix typo in actions workflow config
    
    * add google auth to deploy function jobs
    
    * update entrypoint for workflow deploy functions
    
    * updated format pattern for event trigger resource
    
    * include local development option for functions
    
    * storage bucket name via env var
    
    * remove file delete since it will override
    
    * include log info during processing
    
    * reformat with prettier
    
    * upload file and set custom metadata at same time
    
    * remove reference to storage bucket name
    
    * add firebase config as env var
    
    * single quote for firebase config object env var
    
    * adding missing double quotes
    
    * adding back storage bucket env var
    
    * bug fix
    
    * regex to match file pattern
    
    * add comments
    
    * copy image metadata with resize
    
    * update readme with references

---
+++ function/index.js
@@ -0,0 +1,89 @@
+const functions = require('firebase-functions');
+const { Storage } = require('@google-cloud/storage');
+const { env } = require('process');
+const path = require('path');
+const sharp = require('sharp');
+
+const FUNCTION_ORIGIN = env.FIREBASE_FUNCTION_ORIGIN;
+const STORAGE_BUCKET = env.FIREBASE_STORAGE_BUCKET;
+const IMAGE_MAX_HEIGHT = 450; // pixels
+
+const storage = new Storage();
+
+/**
+ * Determines if image requires processing based on prefix & path pattern
+ * Only certain images should be processed
+ *
+ * @param {string} filePath
+ * @return {boolean}
+ */
+function shouldProcessPrefix(filePath) {
+  const validPrefixes = ['owners', 'pets'];
+  const fileName = path.basename(filePath);
+  // Valid pattern: owners/abc123id/Image_1234.jpg
+  const filePattern = new RegExp(
+    `^(${validPrefixes.join('|')})/\\w+/${fileName}$`
+  );
+  return filePattern.test(filePath);
+}
+
+/**
+ * Determines if image was already processed by checking the metadata
+ *
+ * @param {{[key: string]: string} | undefined} metadata
+ * @return {boolean}
+ */
+function shouldProcessFile(metadata) {
+  return metadata
+    ? metadata?.firebaseFunctionOrigin !== FUNCTION_ORIGIN
+    : true;
+}
+
+/**
+ * Process image when it's uploaded in the Storage bucket
+ * Main entrypoint for the Cloud Function
+ */
+exports.processImage = functions.storage
+  .bucket(STORAGE_BUCKET)
+  .object()
+  .onFinalize((object) => {
+    const fileBucket = object.bucket;
+    const filePath = object.name;
+    const contentType = object.contentType;
+
+    functions.logger.info(`Storage event triggered by: ${filePath}`);
+
+    // Only process files considered to be valid images / photos
+    if (
+      !contentType.startsWith('image/') ||
+      !shouldProcessPrefix(filePath) ||
+      !shouldProcessFile(object.metadata)
+    )
+      return null;
+
+    functions.logger.info('Processing image...');
+
+    const bucket = storage.bucket(fileBucket);
+
+    // Create write stream for the processed image
+    const metadata = {
+      contentType,
+      metadata: {
+        firebaseFunctionOrigin: FUNCTION_ORIGIN
+      }
+    };
+    const uploadStream = bucket
+      .file(filePath)
+      .createWriteStream({ metadata });
+
+    // Resize the height porportionally based on landscape vs portrait
+    const pipeline = sharp();
+    pipeline.resize({ height: IMAGE_MAX_HEIGHT }).withMetadata().pipe(uploadStream);
+
+    // Send the file back to override the original file in the bucket
+    bucket.file(filePath).createReadStream().pipe(pipeline);
+
+    return new Promise((resolve, reject) =>
+      uploadStream.on('finish', resolve).on('error', reject)
+    ).finally(() => functions.logger.info(`Processed: ${filePath}`));
+  });
